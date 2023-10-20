89d0ea809794253e10420375df4408bd9e1c7381
Muhammad Ridho Ananda <ridho.ananda@traveloka.com>
2023-10-20 15:01:13 +0700

    implement bootcamp subtraction rpc

---
+++ bootcamp-subtraction-rpc/src/main/java/com/traveloka/bootcamp/subtraction/SubtractionClientComponent.java
@@ -0,0 +1,38 @@
+package com.traveloka.bootcamp.subtraction;
+
+import com.traveloka.common.concurrent.ConcurrentComponent;
+import com.traveloka.common.http.HttpClientComponent;
+import com.traveloka.common.monitor.OnlineMonitorComponent;
+import org.dk.rpc.client.RpcClientSingleEndpointComponent;
+
+public class SubtractionClientComponent implements SubtractionComponent {
+    private final SubtractionService subtractionService;
+
+    public SubtractionClientComponent(Config config,
+                                      HttpClientComponent httpClientComponent,
+                                      ConcurrentComponent concurrentComponent,
+                                      OnlineMonitorComponent onlineMonitorComponent) {
+        RpcClientSingleEndpointComponent<SubtractionService> subtractionServiceRpcClient = new RpcClientSingleEndpointComponent<>(
+                config.rpcClient,
+                httpClientComponent.getRpcJettyHttpClientFactory(),
+                concurrentComponent.getExecutorFactoryBuilder(),
+                onlineMonitorComponent.getOnlineMonitorListenerProxyFactory(),
+                SubtractionService.class,
+                config.hostname,
+                config.port,
+                "/subtraction");
+
+        subtractionService = subtractionServiceRpcClient.getProxy();
+    }
+
+    @Override
+    public SubtractionService getSubtractionService() {
+        return subtractionService;
+    }
+
+    public static class Config {
+        RpcClientSingleEndpointComponent.Config rpcClient;
+        String hostname;
+        int port;
+    }
+}
